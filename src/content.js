79ef21cdc9fe267e3f5125eafb01622a3710fde1
sayak119 <sk.sayakkundu1997@gmail.com>
2018-07-25 21:25:48 +0530

    fast

---
+++ src/content.js
@@ -0,0 +1,66 @@
+var wordList = read();
+
+addEventListener('scroll',scrollevent);
+function scrollevent(){getTitles();}
+
+function getTitles(){
+  let a_tags = document.getElementsByTagName('a');
+  for(elm of a_tags){
+    if (elm.id == "video-title"){
+      var i =  getrating(elm.title.toLowerCase());
+      elm.innerHTML = ("KV-> " + "<img src=\"https://raw.githubusercontent.com/sayak119/Knowledge-Value/master/icons/logo.png\" height=\"15px\" width=\"15px\">" + i.toString() + "% ").bold().big() + "    " + elm.title;
+    }
+  }
+  let b_tags = document.getElementsByTagName('span');
+      for(elm of b_tags){
+        if (elm.id == "video-title"){
+          var i =  getrating(elm.title.toLowerCase());
+          elm.innerHTML = ("KV->  "+ "<img src=\"https://raw.githubusercontent.com/sayak119/Knowledge-Value/master/icons/logo.png\" height=\"15px\" width=\"15px\">" + i.toString() + "% ").bold().big() + "    " + elm.title;
+        }
+      }
+}
+
+function read(){
+  var txt = '';
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function(){
+  if(xmlhttp.status == 200 && xmlhttp.readyState == 4){}
+  };
+  xmlhttp.open("GET","https://raw.githubusercontent.com/sayak119/Knowledge-Value/master/src/wordList.txt" ,true);
+  xmlhttp.send();
+  return this;
+}
+
+function getrating(title){
+  var rate_count = 1;
+  var titleArray = title.split(/[ ,]+/);
+
+  for(i in wordList){
+    if(title.includes(i.toLowerCase())){
+      rate_count += 1;
+    }
+  }
+  //console.log("TITLE>LENGTH: "+ title.split(/(\s+)/).length);
+  tempRate = rate_count / titleArray.length * 100;
+  if (tempRate > 21){
+    tempRate += 30;
+    if (tempRate >= 100){
+      tempRate = 94.1;
+    }
+    return Math.round(tempRate);
+  }
+  else{
+    return Math.round(tempRate);
+  }
+
+
+}
+
+chrome.runtime.onMessage.addListener(message);
+
+function message(msg,sender,sendResponse){
+  console.log(msg.txt);
+  if(msg.txt === "activate"){
+      getTitles();
+  }
+}
